bcc1b65f1539bc8c0d895aeac077061c8d0d8d73
Arthur Vinicius <viniciusantosarthur2@gmail.com>
2026-05-07 14:22:30 -0300

    Initial commit

---
+++ src/app/components/default-login-layout/default-login-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  imports: [],
+  templateUrl: './default-login-layout.html',
+  styleUrl: './default-login-layout.scss',
+})
+export class DefaultLoginLayout {
+  logoImage = 'assets/log-Git-Gud!.png';
+  bonfireImage = '/assets/main-image-bonfire-removebg-preview.png';
+}
